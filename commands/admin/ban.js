76ccbddd9ef860e7cae986b867c3acf543c96607
LaJBel Ms <71136486+lajbel@users.noreply.github.com>
2021-10-10 19:10:04 +0000

    ban command

---
+++ commands/admin/ban.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "ban",
+    alias: [],
+    run: (client, message, args) => {
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("i ban you haha");
+
+        let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        let reason = args.splice(1).join(" ");
+
+        if (!member.bannable) return message.channel.send("sorry i'm nub");
+
+        member.ban({reason:reason}).then(ban => {
+            return message.channel.send(`i ban ${member.user.username}`);
+        });
+    }
+};
