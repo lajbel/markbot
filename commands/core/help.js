5b7d59ead65243cb57beea4988069775d7dfe1fb
DaInfLoop <92693892+DaInfLoop@users.noreply.github.com>
2021-10-18 18:01:27 +0100

    Update help ofc!

--- commands/core/help.js
+++ commands/core/help.js
@@ -5,14 +5,28 @@ module.exports = {
     alias: [],
     run: (client, message, args) => {
         const commandsArr = [];
+        const commands = {}
 
-        client.commands.map(x => commandsArr.push("`" + x.name + "`"));
+        client.commands.map(x => {
+            commandsArr.push("`" + x.name + "`")
+            commands[x.name] = x
+        });
+        
+        if (!args.length) {
+            const embed = new MessageEmbed()
+            .setDescription(commandsArr.join(" - "))
+            .setTitle("HAHA I am MARK, and this is my COMMANDS")
+            .setColor("0xffe359");
 
-        const embed = new MessageEmbed()
-        .setDescription(commandsArr.join(" - "))
-        .setTitle("HAHA I am MARK, and this is my COMMANDS")
-        .setColor("0xffe359");
-
-        message.channel.send(embed);
+            message.channel.send(embed);
+        } else {
+            if (!commands.includes(args[0].toLowerCase())) return message.channel.send("imagine trying to find out about a non-existant command")
+            const embed = new MessageEmbed()
+            .addFields([
+                {name: "Aliases", value: `**${commands[args[0]].alias.join('** | **') || "None"}**`}
+            ])
+            .setTitle(`!${args[0].toLowerCase()}`)
+            .setColor("0xffe359");
+        }
     }
 };
