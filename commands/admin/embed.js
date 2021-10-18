0dc4f00fcc59ed6395c7aa6ef02ce26e64cf3c98
LaJBel Ms <71136486+lajbel@users.noreply.github.com>
2021-10-18 17:35:18 +0000

    noob haha

--- commands/admin/embed.js
+++ commands/admin/embed.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "embed",
     alias: [],
     run: (client, message, args) => {
-        if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("i ban you haha");
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("you can't use this command you are noob haha");
 
         return message.channel.send("WORK IN PROGRESS. ATTE: LaJBel")
     }
