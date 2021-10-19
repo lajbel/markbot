3efcc459df31e4a28adfee5775ad036161e2df95
LaJBel Ms <71136486+lajbel@users.noreply.github.com>
2021-10-19 17:38:22 +0000

    better embed

--- commands/admin/embed.js
+++ commands/admin/embed.js
@@ -1,14 +1,21 @@
-const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
     name: "embed",
     alias: [],
     run: (client, message, args) => {
-        if(!message.member.hasPermission("ADMINISTRATOR") || message.author.id == "658650587679948820") return message.channel.send("you can't use this command you are noob haha");
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("you can't use this command you are noob haha");
 
         let arr = args.join(' ').split('|')
+
+        const embed = new MesssageEmbed()
+        .setTitle(arr[0])
+        .setColor("0xffe359")
+        .setDescription(arr[1])
+        .setThumbnail(arr[2]);
         
         message.delete();
-        message.channel.send(new Discord.MessageEmbed().setTitle(arr[0]).setDescription(arr[1]).setThumbnail(arr[2]))
+
+        message.channel.send(embed);
     }
 };
