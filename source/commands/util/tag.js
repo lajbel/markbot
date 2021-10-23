99da22c1409ba48abd7ee477bd543988fdb84209
DaInfLoop <92693892+DaInfLoop@users.noreply.github.com>
2021-10-23 18:49:35 +0100

    !tag uses replitdb - part 2

--- source/commands/util/tag.js
+++ source/commands/util/tag.js
@@ -1,19 +1,19 @@
 const Discord = require('discord.js')
-const tags = require('../../tags.json')
-const fs = require('fs')
+const Database = require('@replit/database')
+const db = new Database()
 
 module.exports = {
     name: "tag",
     alias: ["t"],
-    run: (client, message, args) => {
-        const mongoose = client.db;
+    run: async (client, message, args) => {
+        const tags = JSON.parse(await db.get('tags'));
         const $ = args.shift().toLowerCase();
         const tagname = args.shift().toLowerCase()
         if ($ == "create") {
           if (tags[tagname]) return message.channel.send(`Tag ${tagname} has already been made!`)
           tags[tagname] = args.join(' ')
           
-          fs.writeFileSync('../../tags.json', JSON.stringify(tags, null, 2))
+          await db.set('tags', JSON.stringify(tags))
           
           message.channel.send(`Tag ${tagname} successfully created! Use \`!tag ${tagname}\` to use it.`)
         }
