94d590c991e2bd0de323fa038bf3ebb63d5b6155
lajbel <lajbelms@gmail.com>
2022-03-24 22:04:35 -0300

    select specific tip

--- src/commands/tip.ts
+++ src/commands/tip.ts
@@ -1,4 +1,5 @@
 import {
+	ApplicationCommandOptionTypes,
 	Bot,
 	DiscordenoInteraction,
 	Embed,
@@ -6,33 +7,40 @@ import {
 	sendInteractionResponse,
 } from "../../deps.ts";
 
+const tips = [
+	{
+		t: "`npm i kaboom@next` to install the latest dev release that comes with new features (might be unstable and contain breaking change!)",
+	},
+	{
+		t: "```js\nkaboom({\n  burp: true,\n});```",
+	},
+	{
+		t: "mention <@&901298683906240582> for get <#883782079802908772> about kaboom",
+	},
+	{
+		t: "you can use these fonts in your kaboom game without import anything: `apl386`, `apl386o`, `sink`, `sinko`",
+	},
+	{
+		t: "you can sell your Kaboom game thanks to the MIT License",
+	},
+];
+
 export default () => {
 	return {
 		name: "tip",
 		description: "get a tip about the Kaboom World",
-		options: [],
+		options: [
+			{
+				type: ApplicationCommandOptionTypes.Number,
+				name: "number",
+				description: `tip number - 1-${tips.length}`,
+				required: false,
+			},
+		],
 		exe: (bot: Bot, interaction: DiscordenoInteraction) => {
-			const tips = [
-				{
-					t: "`npm i kaboom@next` to install the latest dev release that comes with new features (might be unstable and contain breaking change!)",
-					img: null,
-				},
-				{
-					t: "```js\nkaboom({\n  burp: true,\n});```",
-					img: null,
-				},
-				{
-					t: "mention <@&901298683906240582> for get <#883782079802908772> about kaboom",
-				},
-				{
-					t: "you can use these fonts in your kaboom game without import anything: `apl386`, `apl386o`, `sink`, `sinko`",
-				},
-				{
-					t: "you can sell your Kaboom game thanks to the MIT License",
-				},
-			];
-
-			const tip = tips[Math.floor(Math.random() * tips.length)];
+			const tip =
+				tips[Number(interaction?.data?.options?.[0].value?.toString()) - 1] ||
+				tips[Math.floor(Math.random() * tips.length)];
 
 			const embed: Embed = {
 				color: 0xffe359,
