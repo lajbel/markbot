73b9adc3a66eea54563c051f1b51cb19a27f50fa
lajbel <lajbelms@gmail.com>
2024-11-01 23:04:11 -0300

    chore: update anti invite

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,11 +1,15 @@
 import { client } from "../client.ts";
+import { KAPLAY_TEAM_ROLE_ID } from "../constants.ts";
 import { hasInvite } from "../util/antiInvites.ts";
 
 export default client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
 
     // anti-invites
-    if (hasInvite(message.content) && !await message.member?.roles.get("883786808062787594")) {
+    if (
+        hasInvite(message.content)
+        && !await message.member?.roles.get(KAPLAY_TEAM_ROLE_ID)
+    ) {
         message.channel.send("hey! don't send invites...");
 
         message.delete();
